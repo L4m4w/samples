7eb93bb25412d27116246214a9b47921ecddf59a
L4m4w <larkzhek@gmail.com>
2021-02-21 15:17:55 +0300

    replace is ok

--- first_try.py
+++ first_try.py
@@ -1,2 +1,18 @@
-print('Hello')
-print('hi')
+arr = [1, 2, 3, 4]
+
+'''
+last = arr[-1]
+arr[-1]=arr[0]
+arr[0] = last
+'''
+
+
+def replace_first(array):
+    if len(arr) <=1:
+        return array
+    else:
+        array.append(arr[0])
+        array.pop(0)
+        return array
+
+replace_first(arr)
